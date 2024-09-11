0b1e67f04d6b5fdb816b8af553edab08952d32b9
blindrun <102088614+blindrun@users.noreply.github.com>
2024-09-11 16:57:15 -0500

    Create astrix.js

---
+++ coins/astrix.js
@@ -0,0 +1,9 @@
+var astrix = {
+    name: 'astrix',
+    fullName: 'Astrix Network',
+    symbol: 'ASTRIX',
+    algorithm: 'AstrixHash', // Replace with AstrixHash details
+    port: 34150,  // RPC port for Astrix node
+    getBlockTemplateSupport: true
+};
+module.exports = astrix;
